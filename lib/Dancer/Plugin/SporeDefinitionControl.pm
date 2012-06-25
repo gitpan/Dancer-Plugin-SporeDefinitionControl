e2b047b5d936f934224ba4dac6ccfe95f47ae6b0
Nicolas Oudard <noudard@cpan.org>
2012-06-25 11:39:57 +0000

    Import of NOUDARD/Dancer-Plugin-SporeDefinitionControl-0.11 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-SporeDefinitionControl
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         NOUDARD/Dancer-Plugin-SporeDefinitionControl-0.11.tar.gz
    gitpan-cpan-author:       NOUDARD
    gitpan-cpan-maturity:     released

--- lib/Dancer/Plugin/SporeDefinitionControl.pm
+++ lib/Dancer/Plugin/SporeDefinitionControl.pm
@@ -17,11 +17,11 @@ Dancer Plugin to control validity of route from a Spore configuration file
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 SYNOPSIS
 
@@ -136,7 +136,7 @@ register 'check_spore_definition' => sub {
         my $req = request;
         my %req_params = params;
         die "method request must be defined" unless (defined( $req->method() ) );
-        die "route pattern request must be defined" unless (defined( $req->{_route_pattern} ) );
+        _returned_error( "route pattern request must be defined", 404) unless (defined( $req->{_route_pattern} ) );
 #        my $all_route_pattern = $req->{_route_pattern};
 #my $detail_route_pattern =  split /?/, $route_pattern;
         unless (defined( $rh_path_validation->{$req->method()} ) )
